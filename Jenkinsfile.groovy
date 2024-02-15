9ab1af3b24a1e21b0b63ffc4342179c712c9f122
javierSK <38510237+javierSK@users.noreply.github.com>
2024-02-15 03:48:10 -0300

    try chat gcp solution

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,34 +1,47 @@
+pipeline {
+    agent any
 
-     
-    properties([
-        parameters([
-            booleanParam(name: 'cmdloop', defaultValue: true, description: 'for loop querys'),
-            string(name: 'Query',  defaultValue: '', description: 'query to execute'),
-            string(name: 'Database', defaultValue: '', description: 'Database to interact'),
-            string(name: 'Environment',  defaultValue: '', description: 'Environment where the database is located.')
-        ])
-    ])
+    parameters {
+        string(name: 'Database', defaultValue: '', description: 'Database to interact')
+        string(name: 'Environment',  defaultValue: '', description: 'Environment where the database is located.')
+        booleanParam(name: 'cmdloop', defaultValue: true, description: 'for loop queries')
+        string(name: 'Query',  defaultValue: '', description: 'query to execute')
+    }
 
+    stages {
         stage('user validation') {
             steps {
-                def feedback = input(message: "Doyou have acces?", submitterParameter: 'submiterid')
-                echo "submiter:${feedback}"
-                if(!['javier'].contains(feedback)) {
-                    currentBuild.result = 'ABORTED'
-                    error("submiter:${feedback} does not have access")
+                script {
+                    def feedback = input(message: "Doyou have access?", submitterParameter: 'submiterid')
+                    echo "submitter: ${feedback}"
+                    if (!['javier'].contains(feedback)) {
+                        currentBuild.result = 'ABORTED'
+                        error("submitter: ${feedback} does not have access")
+                    }
                 }
+            }
+        }
 
+        stage('execute query') {
+            steps {
+                script {
+                    echo "database: ${params.Database}"
+                    while (params.cmdloop == true) {
+                        def userInput = input(
+                            id: 'userInput',
+                            message: 'run queries',
+                            parameters: [
+                                [$class: 'TextParameter', name: 'qry', defaultValue: params.Query]
+                            ]
+                        )
+                        try {
+                            sh "${userInput}"
+                        } catch (Exception ex) {
+                            println "error: $ex"
+                        }
+                    }
+                }
             }
         }
-        stage('execute query'){
-            echo "database: ${params.Database}"
-            while(params.cmdloop == true){
-                userInput = input(id: 'userInput', message: 'run querys', parameters: [[$class: 'TextParameter', name: 'qry', defaultValue: params.Query]])
-                try{
-                    sh "${userInput}"
-                }
-                catch(Exception ex){
-                    println "error: $ex"
-                }
-            }
-        }
+    }
+}
