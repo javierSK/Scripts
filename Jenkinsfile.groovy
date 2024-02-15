1fb45341e5b4626b3012a0377d584111102b6d57
javierSK <38510237+javierSK@users.noreply.github.com>
2024-02-15 03:28:55 -0300

    add import

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,6 @@
 
+   
+ import java.util.List<hudson.model.ParameterDefinition  
     properties([
         parameters{
             string(name: 'Database', defaultValue: '', description: 'Database to interact')
